f22af66e5f9215d831533bd718852a59cddef147
Venkat <srivenkatsairaj13@gmail.com>
2024-06-17 12:29:11 +0530

    update vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: "https://workout-buddy-app-nine.vercel.app",
         changeOrigin: true,
         secure: false,
       },

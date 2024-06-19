5e6af7617877c3bd6b941e4f10591ead579cc159
Venkat <srivenkatsairaj13@gmail.com>
2024-06-19 19:25:52 +0530

    add icons assets

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import dbIcon from "../assets/fitness_center.svg";
+import fdIcon from "../assets/grocery.svg";
 
-const Loader = ({icon}) => {
+const Loader = ({ icon }) => {
   return (
-    <div className='loader'>
-          <span className="material-symbols-outlined">{icon}</span>
+    <div className="loader">
+      <img
+        src={icon === "fitness_center" ? dbIcon : fdIcon}
+        className="material-symbols-outlined"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;

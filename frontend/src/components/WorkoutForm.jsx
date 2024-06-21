fffdcca60890f8fc5add73997bd122acc94978e4
Venkat <srivenkatsairaj13@gmail.com>
2024-06-21 23:18:12 +0530

    hide form on mobile

--- frontend/src/components/WorkoutForm.jsx
+++ frontend/src/components/WorkoutForm.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import { useFormContext} from "../hooks/useFormContext";
+import { useFormContext } from "../hooks/useFormContext";
+import { motion } from "framer-motion";
 
-const API = import.meta.env.VITE_API
+const API = import.meta.env.VITE_API;
 const WorkoutForm = () => {
   const { wcontext } = useFormContext();
   const [title, setTitle] = useState("");
@@ -9,6 +10,7 @@ const WorkoutForm = () => {
   const [reps, setReps] = useState("");
   const [error, setError] = useState(null);
   const [emptyFields, setEmptyFields] = useState([]);
+  const [showForm, setShowForm] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -36,37 +38,62 @@ const WorkoutForm = () => {
       wcontext.dispatch({ type: "CREATE_WORKOUT", payload: json });
     }
   };
-  return (
-    <form className="create" onSubmit={handleSubmit}>
-      <h3>Add a new Workout</h3>
 
-      <label>Exercise Title:</label>
-      <div className="input-container">
+  const handleFormVisibilty = () => {
+    setShowForm(!showForm);
+  };
+  return (
+    <>
+      <form
+        className={`create ${showForm ? "" : "hide"}`}
+        onSubmit={handleSubmit}
+      >
+        <h3>Add a new Workout</h3>
+
+        <label>Exercise Title:</label>
+        <div className="input-container">
+          <input
+            type="text"
+            onChange={(e) => setTitle(e.target.value)}
+            value={title}
+            placeholder="e.g. bench press, leg press, pull up ..."
+            className={emptyFields.includes("title") ? "error" : ""}
+          />
+        </div>
+        <label>Load(Kg):</label>
         <input
-          type="text"
-          onChange={(e) => setTitle(e.target.value)}
-          value={title}
-          placeholder="e.g. bench press, leg press, pull up ..."
-          className={emptyFields.includes("title") ? "error" : ""}
+          type="number"
+          onChange={(e) => setLoad(e.target.value)}
+          value={load}
+          className={emptyFields.includes("load") ? "error" : ""}
         />
-      </div>
-      <label>Load(Kg):</label>
-      <input
-        type="number"
-        onChange={(e) => setLoad(e.target.value)}
-        value={load}
-        className={emptyFields.includes("load") ? "error" : ""}
-      />
-      <label>Reps:</label>
-      <input
-        type="number"
-        onChange={(e) => setReps(e.target.value)}
-        value={reps}
-        className={emptyFields.includes("reps") ? "error" : ""}
-      />
-      <button>Add Workout</button>
-      {error && <div className="error">{error}</div>}
-    </form>
+        <label>Reps:</label>
+        <input
+          type="number"
+          onChange={(e) => setReps(e.target.value)}
+          value={reps}
+          className={emptyFields.includes("reps") ? "error" : ""}
+        />
+        <button>Add Workout</button>
+        {error && <div className="error">{error}</div>}
+      </form>
+
+      <motion.button
+        initial={{ x: 100, opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{
+          duration: 1,
+          type: "spring",
+          stiffness: 200,
+          damping: 20,
+          delay: 1.5,
+        }}
+        className="toggle-form"
+        onClick={handleFormVisibilty}
+      >
+        New
+      </motion.button>
+    </>
   );
 };
 
